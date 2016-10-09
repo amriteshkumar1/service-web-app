0997707efa7b938e88a91993e02d005f261583f8
Amritesh kumar <amriteshkumar@Amriteshs-MacBook-Air.local>
2016-10-10 04:27:14 +0530

    code refactor

--- app/home/scripts/homeController.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
-
-injectTapEventPlugin();
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<Main />, document.getElementById('app'));
